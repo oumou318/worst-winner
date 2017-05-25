716c425c98a91b822ca22dd3f2709d4fc34cb1f5
Oumou318 <oumoudiallo318@gmail.com>
2017-05-25 13:57:20 -0400

    routes in index

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../routes/queries')
+// var db = require('../routes/queries')
+var db = require('../routes/queries2');
 
 /* GET home page. */	  //invoked in queries
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getAll', db.getAnswers, db.getQuestions)
+router.get('/:id', db.getAll);
+router.post('/create', db.createQuiz);
+router.delete('/:id', db.deleteAll);
+router.patch('/:id', db.editQuestion);
 
+
+/*router.get('/getQ', db.getQuestions);
+router.get('/getA', db.getAnswers);
+router.post('/cQ', db.createQ);
+router.post('/cA', db.createA);
+router.delete('/:id', db.deleted);
+router.patch('/:id', db.editCreate)*/
 
 module.exports = router;
